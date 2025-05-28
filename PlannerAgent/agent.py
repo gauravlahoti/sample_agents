f4c7e1eddc5449242d4f111efb13031a3eeec3a9
Gaurav Lahoti <gaurav.lahoti25@gmail.com>
2025-05-28 10:57:37 +0530

    Refactor agent instruction imports for clarity
    - Updated imports in agent.py to reference the prompt module directly for agent instructions.
    - Ensured consistent usage of instruction variables across LlmAgent instances for better maintainability.

--- PlannerAgent/agent.py
+++ PlannerAgent/agent.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from google.adk.agents.llm_agent import LlmAgent
 from google.adk.tools import google_search
 
-from PlannerAgent.prompt import ROOT_AGENT_INSTRUCTIONS,IDEA_AGENT_INSTRUCTIONS,REFINER_AGENT_INSTRUCTIONS
+from PlannerAgent import prompt
 
 logging.info("Loading environment variables")
 load_dotenv()
@@ -15,7 +15,7 @@ idea_agent = LlmAgent(
         name="IdeaAgent",
         model=os.getenv("MODEL"),
         description="Brainstorms creative and exciting weekend travel ideas based on the user's budget & destination",
-        instruction=IDEA_AGENT_INSTRUCTIONS,
+        instruction=prompt.IDEA_AGENT_INSTRUCTIONS,
         tools=[google_search],
         disallow_transfer_to_peers=True,
     )
@@ -24,7 +24,7 @@ refiner_agent = LlmAgent(
         name="RefinerAgent",
         model=os.getenv("MODEL"),
         description="Reviews provided travel ideas and select only those that are within the user's budget",
-        instruction=REFINER_AGENT_INSTRUCTIONS,
+        instruction=prompt.REFINER_AGENT_INSTRUCTIONS,
         # tools=[google_search],
         disallow_transfer_to_peers=True,
     )    
@@ -34,6 +34,7 @@ root_agent = LlmAgent(
         name="PlannerAgent",
         model=os.getenv("MODEL"),
         description="Coordinating specialists agents to provide budget friendly trip ideas",
-        instruction=ROOT_AGENT_INSTRUCTIONS,
-        sub_agents=[idea_agent,refiner_agent]
+        instruction=prompt.ROOT_AGENT_INSTRUCTIONS,
+        tools=[google_search],
+        # sub_agents=[idea_agent]
     )
